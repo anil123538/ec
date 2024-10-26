38a385c935c07dd1b75d6ccb659f908cca8190a7
anil123538 <bhattaraianil507@gmail.com>
2024-10-26 03:56:54 -0700

    user api

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,14 @@
 # orders/urls.py
-from django.urls import path
+from django.urls import path,include
 from .views import PlaceOrderView, OrderHistoryView, UpdateOrderStatusView
 
 urlpatterns = [
     path('place/', PlaceOrderView.as_view(), name='place-order'),
     path('history/', OrderHistoryView.as_view(), name='order-history'),
     path('update/<int:id>/', UpdateOrderStatusView.as_view(), name='update-order-status'),
+    
+
+
+   
+
 ]
